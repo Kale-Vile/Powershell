97eb4b3b01e8fe0af1ba79a87b2d55d75f5306e4
kale-vile <kale.vile@woodside.com.au>
2017-09-07 11:54:00 +0800

    new examples

---
+++ Examples/Invoker.ps1
@@ -0,0 +1,73 @@
+﻿<#
+    
+   ____              __          
+  /  _/__ _  _____  / /_____ ____
+ _/ // _ \ |/ / _ \/  '_/ -_) __/
+/___/_//_/___/\___/_/\_\\__/_/   
+                                 
+
+Invokation Script 
+-Check/call for creds
+-Foreach Invoker for a basic AD function, can be replaced with another function  
+-PS Session run as
+-PSobject/Custom array built 
+                                        
+#>
+
+$results = @()
+
+
+if ($Credential.UserName -eq $null)
+{
+$ADMuser = Read-host "Admin Username"
+$securepass=Read-Host "Enter the password" -AsSecureString
+$Credential = New-Object System.Management.Automation.PSCredential $ADMUser, $SecurePass
+""
+}
+
+
+
+Get-PSSession #| Remove-PSSession
+
+"Credentials"
+$Credential
+
+$Session = Get-PSSession 
+if ($Session.Name -ne "AD")
+{
+$ADserver = Read-Host "Enter Domain Controller Name"
+$ADSession = New-PSSession -ComputerName $ADserver -Credential $Credential -Name AD
+}
+
+
+
+
+
+do
+{
+sleep 1
+}
+
+while ($ADSession.State -ne "Available") 
+
+
+$usertxt = read-host "Enter pathto .txt file with users to be scanned" 
+$Content = Get-Content $usertxt
+""
+
+
+foreach ($username in $Content)
+{
+
+
+
+$invoke = Invoke-Command -Session $ADSession {get-aduser $args[0]} -args $username
+
+$results+=$invoke 
+
+
+}
+
+
+
+$results
---
+++ Examples/Ping-script.ps1
@@ -0,0 +1,26 @@
+﻿<#
+
+Bulk ping script for testing connection to a number of devices
+
+#>
+
+
+$Content = read-host "Enter .txt or .csv path with list computer name or IP's to test (eg. C:\ping.txt)"
+
+$names = Get-content $content 
+
+foreach ($name in $names)
+
+{
+
+$testconnection = Test-Connection -ComputerName $name -Count 1 -ErrorAction SilentlyContinue
+
+if ($testconnection)
+
+{Write-Host "$name ping success" -ForegroundColor Green}
+
+else
+
+{Write-Host "$name ping failed" -ForegroundColor Red}
+
+}
